2502ec1e810dc71d343a6d7c8083b6bd4a97ed93
IPMihnev <ivan_mihnev@abv.bg>
2021-02-22 18:46:01 +0200

    Add new files

---
+++ Linked List/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            for (int i = 0; i < 10; i++)
+            {
+                list.AddHead(new Node(i));
+            }
+            list.PrintList();
+
+            list.ReversePrint();
+        }
+    }
+}
