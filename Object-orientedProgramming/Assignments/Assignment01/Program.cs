bf37081ebe1eab0e42e98e1694e76bd766735bbd
IPMihnev <68635667+IPMihnev@users.noreply.github.com>
2022-08-03 09:58:39 +0200

    Update Program.cs

--- Object-orientedProgramming/Assignments/Assignment01/Program.cs
+++ Object-orientedProgramming/Assignments/Assignment01/Program.cs
@@ -1,69 +1,51 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Assignment01
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            // The program is using decimal types for precision.
+            var sol = new Solution();
+            var input = Console.ReadLine();
+            while(input != "end")
+            {
+               var numbers = input.Split(",")
+                             .Where(x => !string.IsNullOrWhiteSpace(x))
+                             .Select(x => int.Parse(x.Trim()))
+                             .ToArray();
+               var result = sol.solution(numbers);
+               Console.WriteLine(result);
+               input = Console.ReadLine();
+            }
+        }
 
-            decimal monthlyTax = decimal.Parse(Console.ReadLine());
-            decimal smsCount = decimal.Parse(Console.ReadLine());
-            decimal mmsCount = decimal.Parse(Console.ReadLine());
-            decimal a1Minutes = decimal.Parse(Console.ReadLine());
-            decimal telenorMinutes = decimal.Parse(Console.ReadLine());
-            decimal vivacomMinutes = decimal.Parse(Console.ReadLine());
-            decimal roamingMinutes = decimal.Parse(Console.ReadLine());
-            decimal countryMB = decimal.Parse(Console.ReadLine());
-            decimal euMB = decimal.Parse(Console.ReadLine());
-            decimal abroadMB = decimal.Parse(Console.ReadLine());
-            decimal otherTaxes = decimal.Parse(Console.ReadLine());
-            decimal discount = decimal.Parse(Console.ReadLine());
-            decimal totalPrice = 0;
-
-            totalPrice += monthlyTax;
-
-            decimal smsPrice = 0;
-            if (smsCount > 0 && smsCount < 50)
+        public class Solution 
+        { 
+            public int solution(int[] A)
             {
-                smsPrice = smsCount * 0.18m;
+               var set = new HashSet<int>();
+               int maxElement = 0;
+           
+               for (int i = 0; i < A.Length; i++)
+               {
+                   set.Add(A[i]);
+                   maxElement = Math.Max(maxElement, A[i]);
+               }
+           
+               if (maxElement != A.Length)
+               {
+                   return 0;
+               }
+           
+               if (set.Count == A.Length)
+               {
+                   return 1;
+               }
+               return 0;
             }
-            else if (smsCount >= 50 && smsCount <= 100)
-            {
-                smsPrice = smsCount * 0.16m;
-            }
-            else if (smsCount > 100)
-            {
-                smsPrice = smsCount * 0.11m;
-            }
-            totalPrice += smsPrice;
-
-            decimal mmsPrice = 0;
-            if (mmsPrice > 0 && mmsPrice < 50)
-            {
-                mmsPrice = mmsCount * 0.25m;
-            }
-            else if (mmsCount >= 50 && mmsCount <= 100)
-            {
-                mmsPrice = mmsCount * 0.23m;
-            }
-            else if (mmsCount > 100)
-            {
-                mmsPrice = mmsCount * 0.18m;
-            }
-            totalPrice += mmsPrice;
-            totalPrice += a1Minutes * 0.03m;
-            totalPrice += telenorMinutes * 0.09m;
-            totalPrice += vivacomMinutes * 0.09m;
-            totalPrice += roamingMinutes * 0.15m;
-            totalPrice += countryMB * 0.02m;
-            totalPrice += euMB * 0.05m;
-            totalPrice += abroadMB * 0.20m;
-            totalPrice += otherTaxes;
-            totalPrice -= discount;
-
-            Console.WriteLine($"Result: {totalPrice:f2}");
         }
     }
 }
